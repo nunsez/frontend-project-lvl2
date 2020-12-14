300afb5c75ec56a53ef5fc29e7b58f794b8e4f8f
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-14 23:55:58 +0700

    1 step

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const gendiff = new Command();
+
+gendiff
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.');
+
+gendiff.parse(process.argv);
