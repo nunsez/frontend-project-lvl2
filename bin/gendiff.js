d870df56a60a823e9fac93654c7b1723561d60c3
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-15 12:49:08 +0700

    2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const gendiff = new Command();
 
 gendiff
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {}); // should fix it
 
 gendiff.parse(process.argv);
