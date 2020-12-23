99d2cc93eeef7e925443bfa322185e64c4863fd9
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-23 18:49:55 +0700

    test code refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import getDifference from '../index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,39 +11,41 @@ const getFixturePath = (filename) =>
 const readFile = (filename) =>
   fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('test gendiff parsers', () => {
-  const expected = readFile('stylish_diff');
+const filepath1 = getFixturePath('nested1');
+const filepath2 = getFixturePath('nested2.json');
+const filepath3 = getFixturePath('nested2.yml');
+const stylishDiff = readFile('stylish_diff');
+const plainDiff = readFile('plain_diff');
 
+describe('json type tests', () => {
   test.each([
-    ['nested1.json', 'nested2.json'],
-    ['nested1.yml', 'nested2.yml'],
-    ['nested1', 'nested2.yml'],
-  ])('test %s %s files', (path1, path2) => {
-    const filepath1 = getFixturePath(path1);
-    const filepath2 = getFixturePath(path2);
-    const received = getDifference(filepath1, filepath2);
+    ['stylish', stylishDiff],
+    ['plain', plainDiff],
+  ])('%s formater', (format, expected) => {
+    const received = genDiff(filepath1, filepath2, format);
 
     expect(received).toEqual(expected);
+  });
+
+  test('json formater', () => {
+    const received = genDiff(filepath1, filepath2, 'json');
+
+    expect(() => JSON.parse(received)).not.toThrow();
   });
 });
 
-describe('test gendiff formatters', () => {
-  const filepath1 = getFixturePath('nested1.json');
-  const filepath2 = getFixturePath('nested2.json');
-
+describe('yml type tests', () => {
   test.each([
-    [undefined, 'stylish_diff'],
-    ['stylish', 'stylish_diff'],
-    ['plain', 'plain_diff'],
-  ])('%s format', (format, diff) => {
-    const received = getDifference(filepath1, filepath2, format);
-    const expected = readFile(diff);
+    ['stylish', stylishDiff],
+    ['plain', plainDiff],
+  ])('%s formater', (format, expected) => {
+    const received = genDiff(filepath1, filepath3, format);
 
     expect(received).toEqual(expected);
   });
 
-  test('json format', () => {
-    const received = getDifference(filepath1, filepath2, 'json');
+  test('json formater', () => {
+    const received = genDiff(filepath1, filepath3, 'json');
 
     expect(() => JSON.parse(received)).not.toThrow();
   });
