4b6738a5e3929c829b63c0ee341058af405cb39e
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-23 17:33:04 +0700

    stylish formatter code refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,33 +1,48 @@
 import _ from 'lodash';
 
-const treeToObject = (tree) => {
-  const result = tree.reduce((acc, item) => {
-    const { key, newValue, state, oldValue, children, value } = item;
-    if (state === 'nested') {
-      _.set(acc, key, treeToObject(children));
-    }
-    if (state === 'unchanged') {
-      _.set(acc, key, value);
-    }
-    if (state === 'removed') {
-      _.set(acc, `-${key}`, value);
-    }
-    if (state === 'added') {
-      _.set(acc, `+${key}`, value);
-    }
-    if (state === 'changed') {
-      _.set(acc, `-${key}`, oldValue);
-      _.set(acc, `+${key}`, newValue);
+const isObject = (value) =>
+  typeof value === 'object' && value !== null && value.constructor !== Array;
+
+const indent = ' '.repeat(2);
+
+const valueToString = (value, depth) => {
+  if (!isObject(value)) {
+    return value;
+  }
+
+  const keys = _.keys(value);
+  // prettier-ignore
+  const result = keys.reduce((acc, key) => {
+    if (acc === '') {
+      return `${key}: ${valueToString(value[key], depth + 2)}`;
     }
 
-    return acc;
-  }, {});
+    return `${acc}\n${indent.repeat(depth + 2)}${key}: ${valueToString(value[key], depth + 2)}`;
+  }, '');
 
-  return result;
+  return `{\n${indent.repeat(depth + 2)}${result}\n${indent.repeat(depth)}}`;
 };
 
-export default (tree) =>
-  JSON.stringify(treeToObject(tree), null, 4)
-    .replace(/"|,/g, '')
-    .replace(/[ ][ ][+]/g, '+ ')
-    .replace(/[ ][ ][-]/g, '- ');
+const convert = {
+  nested: ({ key, children }, depth, iter) =>
+    `${indent.repeat(depth + 1)}${key}: {\n${iter(children, depth + 2)}
+${indent.repeat(depth + 1)}}`,
+  added: ({ key, value }, depth) =>
+    `${indent.repeat(depth)}+ ${key}: ${valueToString(value, depth + 1)}`,
+  removed: ({ key, value }, depth) =>
+    `${indent.repeat(depth)}- ${key}: ${valueToString(value, depth + 1)}`,
+  changed: ({ key, oldValue, newValue }, depth) =>
+    `${indent.repeat(depth)}- ${key}: ${valueToString(oldValue, depth + 1)}
+${indent.repeat(depth)}+ ${key}: ${valueToString(newValue, depth + 1)}`,
+  unchanged: ({ key, value }, depth) =>
+    `${indent.repeat(depth + 1)}${key}: ${valueToString(value)}`,
+};
+
+const stylish = (tree) => {
+  const iter = (nodes, depth) =>
+    nodes.map((node) => convert[node.state](node, depth, iter)).join('\n');
+
+  return `{\n${iter(tree, 1)}\n}`;
+};
+
+export default stylish;
