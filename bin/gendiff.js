c4267fc0672da58e93a0d39488be211008f7eb5f
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-24 19:44:08 +0700

    version update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@ import { program } from 'commander';
 import genDiff from '../index.js';
 
 program
-  .version('0.8.6')
+  .version('0.9.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
