3e961aeeb867f6af867a6cd0a3c560055b2c9aeb
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-12-24 11:55:05 +0700

    replace index.js to src/

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-import parse from './src/parsers.js';
-import format from './src/formatters/index.js';
-import genDiff from './src/gendiff.js';
+import genDiff from './src/index.js';
 
-export default (filepath1, filepath2, formatName = 'stylish') => {
-  const extention1 = path.extname(filepath1).slice(1) || 'json';
-  const extention2 = path.extname(filepath2).slice(1) || 'json';
-
-  const validPath1 = path.resolve(process.cwd(), filepath1);
-  const validPath2 = path.resolve(process.cwd(), filepath2);
-
-  const data1 = fs.readFileSync(validPath1, 'utf-8');
-  const data2 = fs.readFileSync(validPath2, 'utf-8');
-
-  const json1 = parse(data1, extention1);
-  const json2 = parse(data2, extention2);
-
-  const difference = genDiff(json1, json2);
-
-  return format(difference, formatName);
-};
+export default genDiff;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import parse from './parsers.js';
+import format from './formatters/index.js';
+import genDiff from './gendiff.js';
+
+export default (filepath1, filepath2, formatName = 'stylish') => {
+  const extention1 = path.extname(filepath1).slice(1) || 'json';
+  const extention2 = path.extname(filepath2).slice(1) || 'json';
+
+  const validPath1 = path.resolve(process.cwd(), filepath1);
+  const validPath2 = path.resolve(process.cwd(), filepath2);
+
+  const data1 = fs.readFileSync(validPath1, 'utf-8');
+  const data2 = fs.readFileSync(validPath2, 'utf-8');
+
+  const json1 = parse(data1, extention1);
+  const json2 = parse(data2, extention2);
+
+  const difference = genDiff(json1, json2);
+
+  return format(difference, formatName);
+};
